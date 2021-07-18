69126850c7646c1f02b7d4e136da040fb28624e8
Tayyab <heyitstay011@gmail.com>
2021-07-18 19:42:04 +0530

    Rate Limiting

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,17 @@
 const { Router } = require('express');
 const Product = require('../models/product');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit'); 
+
+const apiLimiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 100,
+    message: "Too many requests, try after 5 minutes"
+});
 
 const router = Router();
+
+router.use(apiLimiter);
 
 router.get('/', cors() ,async (req, res) => {
     let limit = req.query.limit ? Number(req.query.limit) : 10;
